4eaee7a3921a950c1c941c8a066417ce239b6996
Nidhi Nayak <111973548+NidhiNayak08@users.noreply.github.com>
2026-08-11 21:40:15 +0530

    LeetCode: 1367. Linked List in Binary Tree

---
+++ leetcode/linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null)return false;
+
+        if(head.val == root.val && match(head, root)){
+            return true;
+        }
+
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    public boolean match(ListNode list, TreeNode root){
+        if(list == null)return true;
+        if(root == null)return false;
+
+        if(list.val!=root.val)return false;
+
+        return match(list.next, root.left) || match(list.next, root.right);
+    }
+}
